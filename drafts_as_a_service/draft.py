fe699e180b94c20514931d602c1d478e647d7084
Rob Dennis <rdennis@gmail.com>
2014-06-15 18:38:08 -0400

    distributing packs

--- drafts_as_a_service/draft.py
+++ drafts_as_a_service/draft.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 import copy
 import random
 
+from collections import deque
+from itertools import cycle
+
 
 class DraftError(Exception):
     """
     generic error with the draft
     """
+
 
 class Pool(object):
     """
@@ -17,7 +21,7 @@ class Pool(object):
     def __init__(self, cards=None):
         self.cards = copy.copy(list(cards or []))
 
-    def deal_packs(self, packs, cards_per_pack=15):
+    def deal_packs(self, packs, cards_per_pack=15, randomize=True):
         """
         a naive booster-draft implementation
         """
@@ -25,9 +29,44 @@ class Pool(object):
             raise DraftError('require {} cards ({} x {}), have {}'.format(
                 packs * cards_per_pack, packs, cards_per_pack, len(self.cards)
             ))
-        random.shuffle(self.cards)
 
-        return [
+        if randomize:
+            random.shuffle(self.cards)
+
+        return deque([
             self.cards[x:x + cards_per_pack]
             for x in xrange(0, packs * cards_per_pack, cards_per_pack)
-        ]
+        ])
+
+
+class Pod(object):
+    def __init__(self, players=None):
+        self.players = copy.copy(list(players or []))
+        self.num_players = len(self.players)
+        self.player_queues = {
+            player: dict(opened=[], unopened=[])
+            for player in self.players
+        }
+
+    def seat_players(self, players=None, randomize=True):
+        players = players or self.players
+
+        if randomize:
+            random.shuffle(players)
+
+        return players
+
+
+class Draft(object):
+    def __init__(self, pod, pool):
+        self.pod = pod
+        self.pool = pool
+
+    def distribute(self):
+        packs = self.pool.deal_packs(3 * self.pod.num_players)
+        assert packs
+        for idx in xrange(3):
+            for player in self.pod.players:
+                unopened = self.pod.player_queues[player]['unopened']
+                unopened.append(packs.popleft())
+
--- drafts_as_a_service/tests/test_draft.py
+++ drafts_as_a_service/tests/test_draft.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+from __future__ import unicode_literals
+
+import copy
+import mock
 import pytest
 
 from drafts_as_a_service import draft
@@ -9,15 +13,54 @@ def card_count():
 
 
 @pytest.fixture
+def player_count():
+    return 8
+
+
+@pytest.fixture
 def cards(card_count):
     return ['Card {}'.format(c) for c in xrange(card_count)]
 
 
-class TestPool(object):
-    @pytest.fixture
-    def pool(self, cards):
-        return draft.Pool(cards)
+@pytest.fixture
+def players(player_count):
+    return ['@player{}'.format(c) for c in xrange(player_count)]
 
+
+@pytest.fixture
+def pool(cards):
+    return draft.Pool(cards)
+
+
+@pytest.fixture
+def mocked_pool(pool, packs, monkeypatch):
+    def return_expected_packs(*args, **kwargs):
+        return copy.copy(packs)
+    monkeypatch.setattr(pool, 'deal_packs', return_expected_packs)
+    return pool
+
+
+@pytest.fixture
+def packs(pool, player_count, card_count):
+    assert card_count % player_count == 0
+    per_pack = card_count / player_count / 3
+    packs = pool.deal_packs(player_count * 3, per_pack, randomize=False)
+    assert len(packs) == player_count * 3
+    assert all(len(pack) == per_pack for pack in packs)
+    return packs
+
+
+@pytest.fixture
+def pod(players):
+    return draft.Pod(players)
+
+
+@pytest.fixture
+def the_draft(pod, mocked_pool):
+    return draft.Draft(pod, mocked_pool)
+
+
+class TestPool(object):
     @pytest.mark.parametrize('packs, num_per_pack', [
         (1, 1),
         (2, 2),
@@ -34,3 +77,43 @@ class TestPool(object):
         with pytest.raises(draft.DraftError):
             pool.deal_packs(card_count / 2 + 1, 2)
 
+
+class TestPod(object):
+    @mock.patch('random.shuffle')
+    def test_default_random_seating(self, mock_shuffle, pod):
+        assert not mock_shuffle.called
+        pod.seat_players()
+        assert mock_shuffle.called
+
+    @mock.patch('random.shuffle')
+    def test_non_random_seating(self, mock_shuffle, pod):
+        assert not mock_shuffle.called
+        pod.seat_players(randomize=False)
+        assert not mock_shuffle.called
+
+    def test_players_start_with_with_queues(self, pod):
+        assert all(q == dict(opened=[], unopened=[])
+                   for p,q in pod.player_queues.iteritems())
+
+
+class TestDraft(object):
+    def test_sanity(self, the_draft, packs):
+        # we mocked this out to always deal the same packs
+        assert the_draft.pool.deal_packs() == packs
+        assert the_draft.pool.deal_packs(123, 'asdf') == packs
+
+    def test_distribute(self, the_draft, packs):
+        the_draft.distribute()
+
+        # this works thanks to the mocking we've set out
+        expected = {
+            '@player{}'.format(idx): {
+                'opened': [],
+                'unopened': [packs[0 + idx],
+                             packs[8 + idx],
+                             packs[16 + idx]],
+            }
+            for idx in xrange(8)
+        }
+
+        assert the_draft.pod.player_queues == expected
